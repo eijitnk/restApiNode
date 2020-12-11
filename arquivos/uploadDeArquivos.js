efccf5060b47a226035aed0840fff411834dfc3e
Eiji <willianeijitanaka@gmail.com>
2020-12-11 04:05:26 -0300

    image

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 // fs.readFile('./assets/mario.png', (erro, buffer) => {
 //   console.log('A imagem foi bufferizada')
@@ -9,6 +10,19 @@ const fs = require('fs')
 //   })
 // });
 
-fs.createReadStream('./assets/mario.png')
-  .pipe(fs.createWriteStream('./assets/mario-bross.png'))
-  .on('finish', () => console.log('Imagem foi escrita com sucesso'))
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+  const tiposValidos = ['jpg', 'png', 'jpeg']
+  const tipo = path.extname(caminho)
+  const tipoEhValido = tiposValidos.indexOf(tipo.substring(1))
+
+  if(tipoEhValido === -1) {
+    console.log('Erro! Tipo inválido')
+  } else {
+    const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+    fs.createReadStream(caminho)
+      .pipe(fs.createWriteStream(novoCaminho))
+      .on('finish', () => callbackImagemCriada(novoCaminho))
+
+  }  
+}
+
