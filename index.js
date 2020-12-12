e9fd7af30a3b480a51a31e2eb7455262d8d521a4
Eiji <willianeijitanaka@gmail.com>
2020-12-11 21:31:01 -0300

    testeConexao

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const customExpress = require('./config/customExpress')
 const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/tabelas')
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 conexao.getConnection(erro => {
     if(erro) {
