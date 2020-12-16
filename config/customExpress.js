bf2b80beee31a454785cd1b81e53b8706986311a
Eiji <willianeijitanaka@gmail.com>
2020-12-16 00:10:17 -0300

    timeout

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const consign = require('consign')
 const bodyParser = require('body-parser')
+const timeout = require('connect-timeout')
 
 module.exports = () => {
     const app = express()
 
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
+    app.use(timeout('5s'))
     
     consign()
         .include('controllers')
