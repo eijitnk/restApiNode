79e04b59f555e50a3ef5b6fd4c2d88e975efcaaa
Eiji <willianeijitanaka@gmail.com>
2020-12-16 03:13:26 -0300

    newTest

--- index.js
+++ index.js
@@ -12,16 +12,6 @@ conexao.getConnection(erro => {
         console.log('conectado com sucesso')
         Tabelas.init(conexao)
         const app = customExpress()        
-        app.listen(port, () => console.log(`servidor rodando em ${port}`))
-
-        app.get('/:cpf', (req, res) => {
-            const { cpf } = req.params
-          
-            res.status(200).json({
-              cpf,
-              nome: faker.name.findName(),
-              dataDeNascimento: faker.date.past()
-            })
-        })
+        app.listen(port, () => console.log(`servidor rodando em ${port}`))        
     }
 })
